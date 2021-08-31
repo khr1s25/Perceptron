e36cd1355b6c051df2d1b58dc61ba16ef4949ccc
Chrostofer Abreu <khristof25@gmail.com>
2021-08-31 01:20:40 -0500

    Reacomodo de archivos dibujo de grafica

--- main.py
+++ main.py
@@ -1,17 +1,10 @@
-# This Python file uses the following encoding: utf-8
-import os
-from pathlib import Path
 import sys
-import pyqtgraph as pg
-from pyqtgraph import PlotWidget, plot
-
-from PySide6.QtWidgets import QApplication, QWidget
-from PySide6.QtCore import QFile
-from PySide6.QtUiTools import QUiLoader
+from mainWindow import Main
+from PySide6.QtWidgets import QApplication
 
 
 if __name__ == "__main__":
-    app = QApplication(sys.argv)
+    app = QApplication([])
     widget = Main()
     widget.show()
     sys.exit(app.exec_())
--- mainWindow.py
+++ mainWindow.py
@@ -1,5 +1,5 @@
 from ui_form import Ui_Main
-from PySide6.QtWidgets import QApplication, QWidget
+from PySide6.QtWidgets import QApplication, QWidget, QGraphicsView, QGraphicsScene
 import os
 from pathlib import Path
 import sys
@@ -13,10 +13,15 @@ class Main(QWidget):
         self.ui.setupUi(self)
 
         # pen = pg.mkPen(color=())
+        self.scene = QGraphicsScene()
+        self.scene.setSceneRect(0, 0, 386, 286)
+        self.ui.Grid.setScene(self.scene)
 
-        self.graph = pg.PlotWidget()
+        self.graph = pg.plot()
+        self.graph.setGeometry(6, 6, 380, 280)
         self.graph.showGrid(x=True, y=True)
-        self.graph.setXRange(-5, 5)
-        self.graph.setYRange(-5, 5)
-        self.ui.Grid.setScene(self.graph)
+        self.graph.setXRange(5, -5)
+        self.graph.setYRange(5, -5)
+        self.scene.addWidget(self.graph)
+
 
