0ac98f35da15ec0663355df6d0bddf4eab7ab9a0
Chrostofer Abreu <khristof25@gmail.com>
2021-08-31 00:44:06 -0500

    Add uiform.py and mainwindow

--- main.py
+++ main.py
@@ -2,28 +2,16 @@
 import os
 from pathlib import Path
 import sys
+import pyqtgraph as pg
+from pyqtgraph import PlotWidget, plot
 
-from PySide2.QtWidgets import QApplication, QWidget
-from PySide2.QtCore import QFile
-from PySide2.QtUiTools import QUiLoader
-
-
-class Main(QWidget):
-    def __init__(self):
-        super(Main, self).__init__()
-        self.load_ui()
-
-    def load_ui(self):
-        loader = QUiLoader()
-        path = os.fspath(Path(__file__).resolve().parent / "form.ui")
-        ui_file = QFile(path)
-        ui_file.open(QFile.ReadOnly)
-        loader.load(ui_file, self)
-        ui_file.close()
+from PySide6.QtWidgets import QApplication, QWidget
+from PySide6.QtCore import QFile
+from PySide6.QtUiTools import QUiLoader
 
 
 if __name__ == "__main__":
-    app = QApplication([])
+    app = QApplication(sys.argv)
     widget = Main()
     widget.show()
     sys.exit(app.exec_())
