947dcf6329481c5ac55926e6044f54c936411962
Chrostofer Abreu <khristof25@gmail.com>
2021-09-02 03:09:50 -0500

    meh

--- mainWindow.py
+++ mainWindow.py
@@ -1,8 +1,8 @@
 from PySide6.QtCore import Slot
 
 import ui_form
+from perceptron import Perceptron
 from PySide6.QtWidgets import QApplication, QWidget, QGraphicsView, QGraphicsScene, QGraphicsSceneMouseEvent
-from random import randint
 import pyqtgraph as pg
 import numpy as np
 from PySide6.QtGui import QPen,QColor, QMouseEvent, Qt
@@ -10,6 +10,8 @@ from PySide6.QtGui import QPen,QColor, QMouseEvent, Qt
 class Main(QWidget):
     def __init__(self):
         super().__init__()
+        self.samples = []
+        self.tipos = []
         self.ui = ui_form.Ui_Main()
         self.ui.setupUi(self)
 
@@ -24,50 +26,35 @@ class Main(QWidget):
         self.scene.addWidget(self.graph)
         self.scene.mousePressEvent = self.click
 
-        # _____--------______---------_________---------________________________--------------
+        self.pesos = np.zeros(self.samples)
 
-    # def perceptron(self):
-    #     for _ in range(self.masIter):
-    #         for idx, x_i in enumerate(self.samples):
-    #             salidaLinear = np.dot(x_i, self.pesos) + self.salidas
-    #             tipoPredecido = self.funcionActivacion(salidaLinear)
-    #
-    #             update = self.learningRate * (self.tipos[idx] - tipoPredecido)
-    #             self.pesos += update * x_i
-    #
-    #
-    # def prueba(self):
-    #     pass
-    #
-    # def prediccion(self):
-    #     salidaLinear =  np.dot(self.samples, self.pesos) + self.salidas
-    #     tipoPredecido = self.funcionActivacion(salidaLinear)
-    #     return tipoPredecido
-    #
-    # def funcionActivacion(self, x):
-    #     return np.where(x>=0, 1, 0)
-    #
-    # @Slot()
-    # def iniciarPesos(self):
-    #     self.pesos = np.random(self.n_features)
-    #     self.salidas = 0
-    #
-    # @Slot()
-    # def click(self, event):
-    #     pen = QPen()
-    #     x = event.scenePos().x()
-    #     y = event.scenePos().y()
-    #     coordenada = {
-    #         "x": x,
-    #         "y": y
-    #     }
-    #     pen.setWidth(2)
-    #     if self.ui.tipo1.isChecked():
-    #         pen.setColor("green")
-    #         self.samples.append(coordenada)
-    #         self.tipos.append(1)
-    #     elif self.ui.tipo2.isChecked():
-    #         pen.setColor("red")
-    #         self.samples.append(coordenada)
-    #         self.tipos.append(0)
-    #     self.scene.addEllipse(x, y, 4, 4, pen)
+        self.p = Perceptron(float(self.ui.learnigRate.text()), int(self.ui.epocasMax.text()), self.pesos)
+        self.ui.iniciarPesos.clicked.connect(self.iniciarPesos)
+        self.ui.iniciarPerceptron.clicked.connect(self.iniciarPerceptron)
+
+    @Slot()
+    def click(self, event):
+        pen = QPen()
+        x = event.scenePos().x()
+        y = event.scenePos().y()
+        coordenada = {
+            "x": x,
+            "y": y
+        }
+        pen.setWidth(2)
+        if self.ui.tipo1.isChecked():
+            pen.setColor("green")
+            self.samples.append(coordenada)
+            self.tipos.append(1)
+        elif self.ui.tipo2.isChecked():
+            pen.setColor("red")
+            self.samples.append(coordenada)
+            self.tipos.append(0)
+        self.scene.addEllipse(x, y, 4, 4, pen)
+
+    @Slot()
+    def iniciarPesos(self):
+        pass
+
+    def iniciarPerceptron(self):
+        self.p.fit(self.samples, self.tipos)
--- perceptron.py
+++ perceptron.py
@@ -3,27 +3,23 @@ import numpy as np
 
 class Perceptron:
 
-    def __init__(self, learning_rate, n_iters):
-        self.lr = learning_rate
-        self.n_iters = n_iters
-        self.activation_func = self._unit_step_func
-        self.weights = None
-        self.bias = None
+    def __init__(self, tazaEntrenamiento, iteraciones, pesos):
+        self.lr = tazaEntrenamiento
+        self.iteraciones = iteraciones
+        self.funcionActivacion = self._unit_step_func
+        self.weights = pesos
+        self.bias = 0
 
     def fit(self, X, y):
         n_samples, n_features = X.shape
 
-        # init parameters
-        self.weights = np.zeros(n_features)
-        self.bias = 0
-
         y_ = np.array([1 if i > 0 else 0 for i in y])
 
-        for _ in range(self.n_iters):
+        for _ in range(self.iteraciones):
 
             for idx, x_i in enumerate(X):
                 linear_output = np.dot(x_i, self.weights) + self.bias
-                y_predicted = self.activation_func(linear_output)
+                y_predicted = self.funcionActivacion(linear_output)
 
                 # Perceptron update rule
                 update = self.lr * (y_[idx] - y_predicted)
@@ -33,7 +29,7 @@ class Perceptron:
 
     def predict(self, X):
         linear_output = np.dot(X, self.weights) + self.bias
-        y_predicted = self.activation_func(linear_output)
+        y_predicted = self.funcionActivacion(linear_output)
         return y_predicted
 
     def _unit_step_func(self, x):
